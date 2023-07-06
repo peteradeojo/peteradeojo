9697472d6c39766455e6de862d90657932cdcae8
Peter Ade-Ojo <60135915+peteradeojo@users.noreply.github.com>
2023-07-06 13:24:49 +0100

    Added new post

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -11,6 +11,12 @@ export default function handler(req, res) {
 			title: "Sending E-mails in PHP with PHPMailer",
 			link: "https://dev.to/peteradeojo/sending-e-mails-in-php-with-phpmailer-15ko",
 			body: "E-mails provide one of the most efficient ways of communication. PHPMailer is a PHP library that allows you to send emails from your PHP application. It is a very popular library and is used by many developers. In this article, I will show you how to use PHPMailer to send emails from your PHP application.",
+		},
+		{
+			id: 3,
+			title: "Unlocking Actionable Transaction Insights: Weighted Averages for Accurate Predictions",
+			link: "https://dev.to/peteradeojo/unlocking-actionable-transaction-insights-weighted-averages-for-accurate-predictions-4c9i",
+			body: "Weighted averages involve assigning different weights to sets of transaction data to denote their relevance. By using this technique, we can calculate the reliability of transactions performed by different financial institutions. Read more to find out how"
 		}
 	];
 	res.status(200).json(JSON.stringify(posts));
